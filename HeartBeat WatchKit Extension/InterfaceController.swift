d2337e21ba1e462cf06da410a76a2e928dc52d8f
Janne Sauvala <janne.sauvala@gmail.com>
2016-02-11 12:01:26 +0200

    Initial Commit

---
+++ HeartBeat WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HeartBeat WatchKit Extension
+//
+//  Created by Janne Sauvala on 11.2.2016.
+//  Copyright © 2016 Janne Sauvala. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ HeartBeat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartBeat
+//
+//  Created by Janne Sauvala on 11.2.2016.
+//  Copyright © 2016 Janne Sauvala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
